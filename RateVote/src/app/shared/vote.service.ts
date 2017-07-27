0ee14a477ef43400c046803f515eee41a72959fc
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-07-27 15:58:00 +0200

    More metadata on given ratings

--- RateVote/src/app/shared/vote.service.ts
+++ RateVote/src/app/shared/vote.service.ts
@@ -10,20 +10,30 @@ export class VoteService {
     private dbService: DbService,
     private authService: AuthService
   ) {
+
   }
 
   vote(pollId: string, pollOption: PollOption, rating: number, maxRating: number) {
+
     if ( ! this.authService.userSaved ) {
       window.alert('No logged user! Cannot give rating yet.')
     } else {
-      let dbObject = this.dbService.objectById(`Ratings/${pollId}/${(<any>pollOption).$key}`,
+      const user = this.authService.userSaved && {
+          displayName: this.authService.userSaved.displayName,
+          id: this.authService.userSaved.uid,
+        }
+      const dbObject = this.dbService.objectById(`Ratings/${pollId}/${(<any>pollOption).$key}`,
         this.authService.userSaved.uid);
 
       const newObject = {
+        pollOption: {
+          title: pollOption.title
+        },
         rating: `${rating}/${maxRating}`,
         history: {
           created: {
             when: new Date().toISOString().replace('T', '_'),
+            byUser: user
           }
         }
       };
