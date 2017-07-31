5285954a1c0a52a9cf12d2b6564b1fe2cd552acd
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-07-31 18:51:26 +0200

    Improved comment timestamp format

--- RateVote/src/app/comment-view/comment-view.component.ts
+++ RateVote/src/app/comment-view/comment-view.component.ts
@@ -11,12 +11,14 @@ export class CommentViewComponent implements OnInit {
 
   @Input() comment: DbObject<Comment>
   commentSaved: Comment
+  whenCreated: string
 
   constructor() { }
 
   ngOnInit() {
     this.comment.subscribe(c => {
       this.commentSaved = c
+      this.whenCreated = c.history.created.when.replace(/:\d\d\.\d\d\dZ/, '').replace('_', ' ')
     })
   }
 
